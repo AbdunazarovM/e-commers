d890290e2459afe5ab71beffd2119d554be4c789
AbdunazarovM <general0320m@gmail.com>
2021-06-22 09:45:09 +0500

    copied from vayvoo

--- js/scripts.js
+++
@@ -1,6 +0,0 @@
- let toglerBtn = document.querySelector('.burger-menu');
- let list = document.querySelector('.site-header__list--kind-of-products');
- 
- toglerBtn.addEventListener('click', function (){
-     list.classList.toggle('list--open');
- })
